69a6a0f2c6075c175b86d11b882cca0af34ea4f5
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-10-29 21:39:27 +0530

    Factory Class Created

---
+++ Factory_Patterns.java
@@ -0,0 +1,13 @@
+import javax.xml.stream.FactoryConfigurationError;
+
+public class Factory_Patterns {
+    public void OS()
+    {
+        System.out.println("This is factory Pattern");
+    }
+}
+
+public static void main(String[] args) {
+    Factory_Patterns obj01 = new
+
+}
